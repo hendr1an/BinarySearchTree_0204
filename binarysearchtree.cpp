4405352e3ed9b51d951d1c7046a15b0f70470a4d
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-06-13 15:35:05 +0700

    commit7

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -55,5 +55,13 @@ public:
         {
             parent->rightchild = newNode;
         }
+    }
+
+
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
+
+        currentNode = ROOT;
+        parent = NULL;
     } 
 };
