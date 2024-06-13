e002eb8509437c47abdcfc8fa642b270cb7f5e25
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-06-13 16:03:17 +0700

    commit14

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -133,5 +133,9 @@ int main()
         cout << "4. perform postorder traversal" << endl;
         cout << "5. exit" << endl;
         cout << "\nEnter your choice (1-5) :";
+
+        char ch;
+        cin >> ch;
+        cout << endl;
     }
 }
