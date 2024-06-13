45c0d23d4dcc4022c2abd839aafeb453c423a21e
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-06-13 16:08:01 +0700

    commit15

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -137,5 +137,39 @@ int main()
         char ch;
         cin >> ch;
         cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            x.insert(word);
+            break;
+        }
+        case '2':
+        {
+            x.inorder(x.ROOT);
+            break;
+        }
+        case '3':
+        {
+            x.preorder(x.ROOT);
+            break;
+        }
+        case '4':
+        {
+            x.postorder(x.ROOT);
+            break;
+        }
+        case '5':
+        {
+            return;
+        }
+  
+  
+  
+        }
     }
 }
