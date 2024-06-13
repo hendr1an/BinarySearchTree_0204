caaa81e307e06b509db118d0c813a1e665df9988
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-06-13 15:55:32 +0700

    commit11

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -86,5 +86,22 @@ public:
             cout << ptr->info << " ";
             inorder(ptr->rightchild);
         }
+    }
+
+    
+    void preorder(Node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
     } 
+
 };
