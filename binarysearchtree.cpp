2581dccc4a9c70b7be61442859f1b23e740165b1
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-06-13 16:02:01 +0700

    commit13

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -105,7 +105,6 @@ public:
     } 
 
 
-    
     void postorder(Node* ptr)
     {
         if (ROOT == NULL)
@@ -121,3 +120,18 @@ public:
         }
     }
 };
+
+int main()
+{
+    BinaryTree x;
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Implement insert operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
+        cout << "3. Perform preorder traversal" << endl;
+        cout << "4. perform postorder traversal" << endl;
+        cout << "5. exit" << endl;
+        cout << "\nEnter your choice (1-5) :";
+    }
+}
